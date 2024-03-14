b492baff87e02e8b03db6f84544e9378291bf01b
AlloysiusM <albrentmanlutac@gmail.com>
2024-03-15 12:28:59 +1300

    added Enemy AI
    Enemy AI can now target player. When the player is outside of the enemy radius, the enemy will roam at the general location the player was last at

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float chaseRadius = 4f;
+    public float roamRadius = 2f;
+    public float minSpeed = 1f;
+    public float smoothTime = 0.5f;
+    private Vector3 lastKnownPlayerPosition;
+    private bool playerLastSeen;
+    private Vector3 currentVelocity = Vector3.zero;
+
+    void Start()
+    {
+        // Initialize the last known player position to the initial position of the enemy
+        lastKnownPlayerPosition = transform.position;
+
+        // Set playerLastSeen to false initially since the player hasn't been seen yet
+        playerLastSeen = false;
+    }
+
+    void Update()
+    {
+        // Get Distance to follow player
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        // If the player is within chase radius, move towards player
+        if (distance < chaseRadius)
+        {
+            MoveTowardsPlayer();
+        }
+        else
+        {
+            // If player was last seen, move towards last known player position
+            if (playerLastSeen)
+            {
+                MoveTowardsLastKnownPosition();
+            }
+            // If player was not last seen, perform random roaming behavior
+            else
+            {
+                RandomRoam();
+            }
+        }
+
+        // Reduce enemy speed when moving to last player position
+        if (playerLastSeen && speed > minSpeed)
+        {
+            Decelerate();
+        }
+    }
+
+    // Move the enemy towards the player
+    void MoveTowardsPlayer()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+
+        // Sets AI angle, allows for directional tracking
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        lastKnownPlayerPosition = player.transform.position;
+        playerLastSeen = true;
+    }
+
+    // Move the enemy towards the last known player position
+    void MoveTowardsLastKnownPosition()
+    {
+        float roamingDistance = Vector2.Distance(transform.position, lastKnownPlayerPosition);
+        if (roamingDistance > roamRadius)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, lastKnownPlayerPosition, ref currentVelocity, smoothTime, speed);
+        }
+        else
+        {
+            playerLastSeen = false;
+        }
+    }
+
+    // Perform random roaming behavior
+    void RandomRoam()
+    {
+        float randomX = UnityEngine.Random.Range(-5f, 5f);
+        float randomY = UnityEngine.Random.Range(-5f, 5f);
+        Vector3 randomDirection = new Vector3(randomX, randomY, 0);
+        transform.position = Vector3.SmoothDamp(transform.position, randomDirection, ref currentVelocity, smoothTime, speed);
+    }
+
+    // Gradually reduce speed as it approaches last known player position
+    void Decelerate()
+    {
+        speed -= 0.1f * Time.deltaTime;
+        speed = Mathf.Max(speed, minSpeed);
+    }
+}
